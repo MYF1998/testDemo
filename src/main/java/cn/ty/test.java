dd3ac31027478126e29a7a7c8f17bb3644b1e2b0
MYF1998 <2900764458@qq.com>
2022-06-30 19:29:40 +0800

    wwwwwwwwwwwww

--- src/main/java/cn/ty/test.java
+++ src/main/java/cn/ty/test.java
@@ -3,5 +3,6 @@ package cn.ty;
 public class test {
     public static void main(String[] args) {
         System.out.println("q11111111111");
+        System.out.println("wwwwwwwwwwwwww");
     }
 }
